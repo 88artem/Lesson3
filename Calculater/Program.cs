1d1a0bbb0a0598304cbaee21ab02820cc5ef46b9
88artem <147190913+88artem@users.noreply.github.com>
2025-10-24 16:09:05 +0500

    Update Program.cs

--- Calculater/Program.cs
+++ Calculater/Program.cs
@@ -1,32 +1,25 @@
 ﻿using Calculater;
 using System.Runtime.InteropServices.Marshalling;
 UICalculate _uiCalculate = new();
-double nums1;
-double nums2;
-string symbol = "";
 while (true)
 {
     Console.WriteLine("Введите операцию которую хотите совершить, а затем введите значения!");
-    symbol = Console.ReadLine();
-    nums1 = float.Parse(Console.ReadLine());
-    nums2 = float.Parse(Console.ReadLine());
     try
     {
-        _uiCalculate.Nums(nums1, symbol);
+        _uiCalculate.Nums(float.Parse(Console.ReadLine(), Console.ReadLine();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{ex.Message}");
+    }
+    try
+    {
+        _uiCalculate.Nums(float.Parse(Console.ReadLine(), float.Parse(Console.ReadLine(), Console.ReadLine();
     }
     catch (Exception ex)
     {
         Console.WriteLine($"{ex.Message}");
     }
 
-
-    try
-    {
-        _uiCalculate.Nums(nums1, nums2, symbol);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine($"{ex.Message}");
-    }
 
 }
